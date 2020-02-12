0df972f97788b91b83b0e9447d0db18ebe7b1d58
kkuop <60794860+kkuop@users.noreply.github.com>
2020-02-12 10:44:23 -0600

    added a nested if statement in the RunGame method that will display the actual word instead of the character... also removed half of the else ifs that were not needed

--- RockPaperScissors/Game.cs
+++ RockPaperScissors/Game.cs
@@ -28,76 +28,80 @@ namespace RockPaperScissors
                 Console.WriteLine("\n\nPlayer 2:");
                 char playerTwoChoice = playerTwo.Choice();
 
-
-                if ((playerOneChoice == 'a' && playerTwoChoice == 'c') || (playerOneChoice == 'a' && playerTwoChoice == 'd'))
+                if (playerOneChoice == playerTwoChoice)
+                {
+                    Console.WriteLine("The round ended in a tie!");
+                }
+                else if ((playerOneChoice == 'a' && playerTwoChoice == 'c') || (playerOneChoice == 'a' && playerTwoChoice == 'd'))
                 {
                     playerOne.score++;
-                    Console.WriteLine($"\n{playerOneChoice} beats {playerTwoChoice}");
+                    if (playerTwoChoice == 'c')
+                    {
+                        Console.WriteLine("Rock beats Scissors!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Rock beats Lizard!");
+                    }
+                    Console.WriteLine($"\nThe score is:\nPlayer 1: {playerOne.score}\nPlayer 2: {playerTwo.score}");
                     
                 }
                 else if ((playerOneChoice == 'b' && playerTwoChoice == 'a') || (playerOneChoice == 'b' && playerTwoChoice == 'e'))
                 {
                     playerOne.score++;
-                    Console.WriteLine($"\n{playerOneChoice} beats {playerTwoChoice}");
-                    
+                    if (playerTwoChoice == 'a')
+                    {
+                        Console.WriteLine("Paper beats Rock!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Paper beats Spock!");
+                    }
+                    Console.WriteLine($"\nThe score is:\nPlayer 1: {playerOne.score}\nPlayer 2: {playerTwo.score}");
 
                 }
                 else if ((playerOneChoice == 'c' && playerTwoChoice == 'b') || (playerOneChoice == 'c' && playerTwoChoice == 'd'))
                 {
                     playerOne.score++;
-                    Console.WriteLine($"\n{playerOneChoice} beats {playerTwoChoice}");
-                    
+                    if (playerTwoChoice == 'b')
+                    {
+                        Console.WriteLine("Scissors beats Paper!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Scissors beats Lizard!");
+                    }
+                    Console.WriteLine($"\nThe score is:\nPlayer 1: {playerOne.score}\nPlayer 2: {playerTwo.score}");
 
                 }
                 else if ((playerOneChoice == 'd' && playerTwoChoice == 'e') || (playerOneChoice == 'd' && playerTwoChoice == 'b'))
                 {
                     playerOne.score++;
-                    Console.WriteLine($"\n{playerOneChoice} beats {playerTwoChoice}");
-                    
+                    if (playerTwoChoice == 'e')
+                    {
+                        Console.WriteLine("Lizard beats Spock!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Lizard beats Paper!");
+                    }
+                    Console.WriteLine($"\nThe score is:\nPlayer 1: {playerOne.score}\nPlayer 2: {playerTwo.score}");
 
                 }
                 else if ((playerOneChoice == 'e' && playerTwoChoice == 'a') || (playerOneChoice == 'e' && playerTwoChoice == 'c'))
                 {
                     playerOne.score++;
-                    Console.WriteLine($"\n{playerOneChoice} beats {playerTwoChoice}");
-                    
+                    if (playerTwoChoice == 'a')
+                    {
+                        Console.WriteLine("Spock beats Rock!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Spock beats Scissors!");
+                    }
+                    Console.WriteLine($"\nThe score is:\nPlayer 1: {playerOne.score}\nPlayer 2: {playerTwo.score}");
 
-                }
-                else if ((playerTwoChoice == 'a' && playerOneChoice == 'c') || (playerTwoChoice == 'a' && playerOneChoice == 'd')) 
-                {
-                    playerTwo.score++;
-                    Console.WriteLine($"\n{playerTwoChoice} beats {playerOneChoice}");
-                    
-
-                }
-                else if ((playerTwoChoice == 'b' && playerOneChoice == 'a') || (playerTwoChoice == 'b' && playerOneChoice == 'e'))
-                {
-                    playerTwo.score++;
-                    Console.WriteLine($"\n{playerTwoChoice} beats {playerOneChoice}");
-                    
-
-                }
-                else if ((playerTwoChoice == 'c' && playerOneChoice == 'b') || (playerTwoChoice == 'c' && playerOneChoice == 'd'))
-                {
-                    playerTwo.score++;
-                    Console.WriteLine($"\n{playerTwoChoice} beats {playerOneChoice}");
-                    
-
-                }
-                else if ((playerTwoChoice == 'd' && playerOneChoice == 'e') || (playerTwoChoice == 'd' && playerOneChoice == 'b'))
-                {
-                    playerTwo.score++;
-                    Console.WriteLine($"\n{playerTwoChoice} beats {playerOneChoice}");
-                    
-
-                }
-                else if ((playerTwoChoice == 'e' && playerOneChoice == 'a') || (playerTwoChoice == 'e' && playerOneChoice == 'c'))
-                {
-                    playerTwo.score++;
-                    Console.WriteLine($"\n{playerTwoChoice} beats {playerOneChoice}");
-                    
-
-                }
+                }                
                 else
                 {
                     Console.WriteLine("\nI don't have a rule for that yet...");
